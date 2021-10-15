74dcb0efa24ed4b070c45926bf42f1ed2186c751
Philippe Milot <phil@philmilot.com>
2021-10-15 00:24:37 -0400

    Render video

--- operatic.cpp
+++ operatic.cpp
@@ -4,6 +4,7 @@
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keyboard.h>
+#include <SDL2/SDL_ttf.h>
 
 #include "dbopl.h"
 
@@ -25,6 +26,13 @@ enum channel_param
 	CHANNEL_PARAM_COUNT
 };
 
+const char * channel_param_str[CHANNEL_PARAM_COUNT] = {
+	"(P)itch / F-number",
+	"(F)eedback",
+	"(O)ctave",
+	"Key-On"
+};
+
 uint16_t channel_param_mask[CHANNEL_PARAM_COUNT] = {
 	0x03ff, // f-number: 10 bits
 	0x0007, // feedback: 3 bits
@@ -37,6 +45,19 @@ struct channel_state_t
 	uint16_t params[CHANNEL_PARAM_COUNT];
 };
 
+struct app_renderer_t
+{
+	SDL_Window * window;
+	SDL_Renderer * renderer;
+	SDL_Surface * surface;
+	SDL_Rect dim;
+	TTF_Font * font;
+	int x;
+	int y;
+	int lineheight;
+	int lineskip;
+};
+
 struct app_state_t
 {
 	channel_state_t channels[18];
@@ -46,6 +67,7 @@ struct app_state_t
 	uint8_t current_param;
 	Handler synth;
 	Bit32s buffer[kBufferSize];
+	app_renderer_t render_state;
 	bool bContinue;
 } app_state;
 
@@ -94,6 +116,10 @@ void write_register(app_state_t &state, Bit32u addr, Bit32u reg, Bit8u val)
 	state.synth.WriteReg(state.synth.WriteAddr(addr, reg), val);
 }
 
+int init_video(app_state_t &app);
+void term_video(app_state_t &app);
+void render_video(app_state_t &app);
+
 int main()
 {
 	app_state = app_state_t{};
@@ -105,24 +131,7 @@ int main()
 		return -1;
 	}
 
-	SDL_Window * window = SDL_CreateWindow(
-		"operatic",                         // window title
-		SDL_WINDOWPOS_UNDEFINED,           // initial x position
-		SDL_WINDOWPOS_UNDEFINED,           // initial y position
-		320,                         // width, in pixels
-		200,                        // height, in pixels
-		SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI // flags - see below
-	);
-	if (window == nullptr) {
-		fprintf(stderr, "Could not create window: %s", SDL_GetError());
-		SDL_Quit();
-		return -1;
-	}
-
-	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) {
-		fprintf(stderr, "Could not create renderer: %s", SDL_GetError());
-		SDL_Quit();
+	if (0 != init_video(app_state)) {
 		return -1;
 	}
 
@@ -226,19 +235,129 @@ int main()
 				}
 			}
 			synth_lock.unlock();
+
+			render_video(app_state);
 		}
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_Rect rect{0, 0, 320, 200};
-		SDL_RenderFillRect(renderer, &rect);
-		SDL_RenderPresent(renderer);
 	}
 	printf("Rendering complete.\n");
 
 	// Clean up
 	SDL_CloseAudioDevice(aid);
 
+	term_video(app_state);
+
 	SDL_Quit();
 
 	return 0;
+}
+
+void render_line(app_state_t &app, const char * msg, SDL_Color * color)
+{
+	SDL_Rect srcrect{0, 0, 0, 0};
+	TTF_SizeText(app.render_state.font, msg, &srcrect.w, &srcrect.h);
+	SDL_Rect dstrect{app.render_state.x, app.render_state.y, srcrect.w, srcrect.h};
+	SDL_Surface * text = TTF_RenderText_Solid(app.render_state.font, msg, *color);
+	if (text != nullptr) {
+		SDL_BlitSurface(text, &srcrect, app.render_state.surface, &dstrect);
+		app.render_state.x = 0;
+		app.render_state.y += srcrect.h + app.render_state.lineskip;
+	} else {
+		fprintf(stderr, "Could not render text: %s\n", TTF_GetError());
+	}
+
+}
+
+void render_video(app_state_t &app)
+{
+	SDL_Renderer * renderer = app.render_state.renderer;
+	SDL_RenderClear(renderer);
+
+	// Draw black
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderFillRect(renderer, &app.render_state.dim);
+
+	SDL_LockSurface(app.render_state.surface);
+	SDL_memset(app.render_state.surface->pixels, 0, app.render_state.dim.h * app.render_state.surface->pitch);
+	SDL_UnlockSurface(app.render_state.surface);
+
+	SDL_Color normal = SDL_Color{192, 192, 192, 255};
+	SDL_Color selected = SDL_Color{192, 255, 192, 255};
+	app.render_state.x = 0;
+	app.render_state.y = 0;
+	char msg[1024];
+	sprintf(msg, "Channel: #%d", app.current_channel);
+	render_line(app, msg, &normal);
+	for (int i=0; i < CHANNEL_PARAM_COUNT; i++) {
+		SDL_Color * color = app_state.current_param == i ? &selected : &normal;
+		sprintf(msg, "  %s: 0x%04x", channel_param_str[i], app_state.channels[app_state.current_channel].params[i]);
+		render_line(app, msg, color);
+	}
+
+	render_line(app, "", &normal);
+	render_line(app, "Press F1-F12 to select a channel", &normal);
+	render_line(app, "Press letter shortcut to select a parameter", &normal);
+	render_line(app, "Use the arrow up/down keys to change parameter values", &normal);
+	render_line(app, "Press spacebar for Note ON/OFF", &normal);
+
+
+	// Render whole texture
+	SDL_Texture * tex = SDL_CreateTextureFromSurface(renderer, app_state.render_state.surface);
+	SDL_RenderCopy(renderer, tex, nullptr, &app.render_state.dim);
+	SDL_RenderPresent(renderer);
+}
+
+int init_video(app_state_t &app)
+{
+	if (-1 == TTF_Init())
+	{
+		fprintf(stderr, "Coult not initialize TTF: %s\n", TTF_GetError());
+		return -1;
+	}
+
+	app.render_state.font = TTF_OpenFont("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 16);
+	if (app.render_state.font == nullptr)
+	{
+		fprintf(stderr, "Could not load font: %s\n", TTF_GetError());
+		return -1;
+	}
+
+	app.render_state.lineheight = TTF_FontHeight(app_state.render_state.font);
+	app_state.render_state.lineskip = TTF_FontLineSkip(app_state.render_state.font);
+
+	app_state.render_state.dim = {0, 0, 800, 600};
+	app_state.render_state.window = SDL_CreateWindow(
+		"operatic",                         // window title
+		SDL_WINDOWPOS_UNDEFINED,           // initial x position
+		SDL_WINDOWPOS_UNDEFINED,           // initial y position
+		app_state.render_state.dim.w,                         // width, in pixels
+		app_state.render_state.dim.h,                        // height, in pixels
+		SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI // flags - see below
+	);
+	if (app_state.render_state.window == nullptr) {
+		fprintf(stderr, "Could not create window: %s", SDL_GetError());
+		SDL_Quit();
+		return -1;
+	}
+
+	app.render_state.renderer = SDL_CreateRenderer(app.render_state.window, -1, SDL_RENDERER_PRESENTVSYNC);
+	if (app.render_state.renderer == nullptr) {
+		fprintf(stderr, "Could not create renderer: %s", SDL_GetError());
+		SDL_Quit();
+		return -1;
+	}
+
+	app_state.render_state.surface = SDL_CreateRGBSurface(0, app_state.render_state.dim.w, app_state.render_state.dim.h, 32, 0, 0, 0, 255);
+	if (app_state.render_state.surface == nullptr) {
+		fprintf(stderr, "Could not create surface: %s", SDL_GetError());
+		SDL_Quit();
+		return -1;
+	}
+	return 0;
+}
+
+void term_video(app_state_t &app)
+{
+	TTF_CloseFont(app.render_state.font);
+
+	TTF_Quit();
 }
